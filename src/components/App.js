21c8f4415ae4143aa5906d83d8f9ec40a9d14b58
marwa <marwajawad19@gmail.com>
2021-08-05 04:57:03 -0400

    Displaying the 🎬

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,47 @@
+import { useState } from "react";
 import video from "../data/video.js";
 
 function App() {
-  console.log("Here's your data:", video);
+  const {title,embedUrl,views,createdAt,downvotes,upvotes,comments} =video
 
+  console.log("Here's your data:", video);
+  const [Like,setLike]=useState(upvotes)
+  
+  const [disLike,setDisLike]=useState(downvotes)
+const [isHide,setIsHide]=useState(true)
   return (
-    <div className="App">
-      <iframe
-        width="919"
-        height="525"
-        src="https://www.youtube.com/embed/dQw4w9WgXcQ"
+    <div className="App"><iframe
+        width="719"
+        height="425"
+        src={embedUrl}
         frameborder="0"
         allowfullscreen
         title="Thinking in React"
       />
+<h1>{title}</h1>
+<p>{views+ " Views"+ " | Uploaded "+createdAt}</p>
+      <div >
+        <button onClick={()=>setLike(perv=>perv+1)}> {Like +" "} 👍</button>
+      
+      <button onClick={()=>setDisLike(perv=>perv-1)}>{disLike +" "} 👎</button></div>
+  
+  <button onClick={()=>setIsHide(!isHide)} style={{marginTop:"10px"}}>
+    {!isHide?"Show Comments":"Hide Comments"}
+    
+    
+    </button>
+    <hr/>
+    {isHide?
+    <div>
+      <h1>{comments.length}{" Comments"}</h1>
+   { comments.map((item,i)=>
+    (<div>
+      <h4>{item.user}</h4>
+      <p>{item.comment}</p>
+    </div>
+    ))}</div>:null}
+    
+    
     </div>
   );
 }
